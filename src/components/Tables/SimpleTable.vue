097f27c8d1542924a84c9416ee53cc39488d23f5
Umur ABALI <umurabali@gmail.com>
2018-07-12 09:36:17 +0300

    Initial commit

---
+++ src/components/Tables/SimpleTable.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div>
+    <md-table v-model="info" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row"  slot-scope="{ item }">
+        <md-table-cell md-label="Company Name">{{ item.companyName }}</md-table-cell>
+        <md-table-cell md-label="Address">{{ item.customerAddress }}</md-table-cell>
+        <md-table-cell md-label="Item">{{ item.orderedItem }}</md-table-cell>
+        <md-table-cell md-label="Cost">{{ item.orderCost }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+    <div style="float:right;margin:10px;">
+      <md-button class="md-primary ">Create</md-button>
+      <md-button class="md-primary ">Update</md-button>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'simple-table',
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      info: []
+    }
+  },
+  created () {
+    axios
+      .get('http://localhost:4040/api/orders')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
+
---
+++ src/pages/Graph.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Material Dashboard Heading</h4>
+            <p class="category">Created using Roboto Font Family</p>
+          </md-card-header>
+          <md-card-content>
+           Hello!
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+  export default{
+    props: {
+      dataBackgroundColor: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+  function onLoad() {
+    var g = Viva.Graph.graph();
+    g.addNode('1','User name 1');
+    g.addNode('2','User name 2');
+    g.addNode('3','User name 3');
+    g.addLink('1', '2');
+    g.addLink('2', '3');
+    g.addLink('3', '1');
+    var renderer = Viva.Graph.View.renderer(g);
+    renderer.run();
+    //list each nodes from the graph
+    g.forEachNode(function(node){
+      console.log(node.id, node.data);
+    });
+    // show the connected nodes with user id 1
+    g.forEachLinkedNode('1', function(linkedNode, link){
+      console.log("Connected node: ", linkedNode.id, linkedNode.data);
+      console.dir(link); // link object itself
+    });
+  }
+
+</script>
